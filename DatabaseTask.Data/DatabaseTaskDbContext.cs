0af7fc0ed0771499eea63cb05171ba0bdb6eb35a
Karl <Karl@DESKTOP-ECBK3O2>
2023-03-20 16:06:04 +0200

    Add project files.

---
+++ DatabaseTask.Data/DatabaseTaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DatabaseTask.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DatabaseTask.Data
+{
+    public class DatabaseTaskDbContext : DbContext
+    {
+        public DatabaseTaskDbContext(DbContextOptions<DatabaseTaskDbContext> options)
+            : base(options) { }
+
+        // näide, kuidas teha, kui lisate domaini alla ühe objekti
+        // migratsioonid peavad tulema siia libary-sse e TARge20.Data alla.
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<Specialist> Specialist { get; set; }
+        public DbSet<ConstructionCompany> ConstructionCompany { get; set; }
+    }
+}
